def671dc2a2e8cb398ac68deed4935fa2bc959b6
carlosallexandre <carlosallexandre@pop-os.localdomain>
2021-09-15 02:17:14 -0300

    perf: ProductItem memoization

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,13 +1,26 @@
+/**
+ * Occasions to apply memo:
+ * 1. Pure functional components
+ * 2. Renders too often
+ * 3. Re-rendes with same props
+ * 4. Medium to big size
+ */
+
+import { memo } from "react";
 import { Product } from "../types/Product";
 
 interface ProductItemProps {
   product: Product;
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   )
 }
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+})
